e61b2e7006699cc4dfb8b60bfeb2c4546aed3705
Marco Seifert <marco.seifert@einstein.com>
2024-06-01 14:22:42 +0200

    add tmux

--- modules/core/core.nix
+++ modules/core/core.nix
@@ -28,6 +28,7 @@ in
     pkgs.fzf
     pkgs.vim
     pkgs.bat
+    pkgs.tmux
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
